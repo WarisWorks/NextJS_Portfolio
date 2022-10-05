ccae090495ca2aa2c7abbc0e076f4013465e23a1
Waris Works <warisruzi@gmail.com>
2022-10-05 15:11:31 +0900

    git new

--- next.config.js
+++ next.config.js
@@ -2,27 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader:  "akamai",
+    path: "",
+  }
 }
 
-module.exports = {
-  assetPrefix: '/nextjs_portfolio',
-};
 
 module.exports = nextConfig
 
-
-const withFonts = require('next-fonts');
-
-module.exports = withFonts({
-   enableSvg: true,
-   webpack(config, options) {
-     return config;
-   }
-});
-
-module.exports = {
-  i18n: {
-    locales: ['ug', 'en'],
-    defaultLocale: 'ug',
-  },
-}
